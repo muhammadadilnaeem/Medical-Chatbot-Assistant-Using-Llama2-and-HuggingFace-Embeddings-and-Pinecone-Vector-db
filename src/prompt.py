1a90ea8a07f5f44b2edc1102abe8175422b30d18
adil <madilnaeem0@gmail.com>
2024-08-08 21:34:56 +0500

    this file contained prompt as a promptemplate

---
+++ src/prompt.py
@@ -0,0 +1,13 @@
+from langchain_core.prompts import PromptTemplate
+
+# Define prompt template for the model 
+prompt_template = """
+Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
