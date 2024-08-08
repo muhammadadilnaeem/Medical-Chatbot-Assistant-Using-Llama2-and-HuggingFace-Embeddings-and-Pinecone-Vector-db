d6eeacfc99648bd435453919a36c787ec0ed0ca5
adil <madilnaeem0@gmail.com>
2024-08-08 21:30:41 +0500

    this  file is used to setup project details and connection

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="Medical Chatbot Implementation",
+    version="0.0.1",
+    author="Adil Naeem",
+    packages=find_packages(),
+    install_requires=[],
+)
